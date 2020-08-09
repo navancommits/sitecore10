9da8304b704c5b87680ccb7b03f480a2ac5bbca9
NAVAN <contactnavaneeth@gmail.com>
2020-08-09 11:51:30 +1000

    for xp0 topology

--- GenerateTLSSSLCerts.ps1
+++ GenerateTLSSSLCerts.ps1
@@ -1,5 +1,5 @@
 IF NOT EXIST mkcert.exe powershell Invoke-WebRequest https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-windows-amd64.exe -UseBasicParsing -OutFile mkcert.exe
 mkcert -install
 del /Q /S traefik\certs\*
-mkcert -cert-file traefik\certs\xm1cm.localhost.crt -key-file traefik\certs\xm1cm.localhost.key "xm1cm.localhost"
-mkcert -cert-file traefik\certs\xm1id.localhost.crt -key-file traefik\certs\xm1id.localhost.key "xm1id.localhost"
+mkcert -cert-file traefik\certs\xp0cm.localhost.crt -key-file traefik\certs\xp0cm.localhost.key "xp0cm.localhost"
+mkcert -cert-file traefik\certs\xp0id.localhost.crt -key-file traefik\certs\xp0id.localhost.key "xp0id.localhost"
